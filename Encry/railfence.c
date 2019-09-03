af4dc89218920a161ed53b3d7b2edbe392474779
student <student@localhost.localdomain>
2019-09-03 10:40:18 -0400

    Railfence Finished

--- Encry/railfence.c
+++ Encry/railfence.c
@@ -1,26 +1,60 @@
 #include<stdio.h>
 #include<string.h>
 
-void encrypt(char *text,char *ciph,int k)
+void encrypt(char *text,int r,int c,char ciph_mat[r][c],char *ciph)
 {
+    int i,j,ci_ind=0;
+//     printf("r:%d c:%d\n",r,c);
+    int index[r];
+    for(i=0;i<r;i++)
+        index[i]=0;
+    int len=strlen(text);
     
+    for(i=0;i<len;i++)
+    {
+        ciph_mat[i%r][index[i%r]]=text[i];
+//         printf("ciph_mat[%d][%d]: %c\n",i%r,index[i%r],ciph_mat[i%r][index[i%r]]);
+        index[i%r]++;
+    }
+    
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            ciph[ci_ind++]=ciph_mat[i][j];
+        }
+    }
+      printf("ci_ind: %d\n",ci_ind);
+      ciph[ci_ind]='\0';
 }
 
 int main()
 {
-    int key,i;
-    char text[100],cipher[100];
+    int key,i,col,text_ind=0;
+    char text[100],msg[100],ciph[100];
     printf("Enter key:");
     scanf("%d",&key);
     getchar();
     printf("Enter text: ");
-    scanf("%[^\n]s",text);
-    printf("%s",text);
-    i=100;
+    scanf("%[^\n]s",msg);
+    int x=strlen(msg);
+    for(i=0;i<x;i++)
+    {
+        if(msg[i]>='a'&&msg[i]<='z')
+            text[text_ind++]=msg[i];
+    }
+    text[text_ind]='\0';
+//     printf("pt: %s\n",text);
+    i=strlen(text);
     while(i%key!=0)
     {
         i++;
     }
-    printf("i: %d\n",i);
+    col=i/key;
+    
+//     printf("i: %d,i/key: %d,key: %d\n",i,col,key);
+    char cipher_mat[key][col];
+    encrypt(text,key,col,cipher_mat,ciph);
+    printf("Cipher: %s\n",ciph);
     return 0;
 }
